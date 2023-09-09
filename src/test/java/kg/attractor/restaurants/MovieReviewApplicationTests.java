6492f39458f906fdae5d37c935aa129ddd0b43b6
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-09-09 18:57:34 +0600

    Initial commit

---
+++ src/test/java/kg/attractor/restaurants/MovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.restaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
